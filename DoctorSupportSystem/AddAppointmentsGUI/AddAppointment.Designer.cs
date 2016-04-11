4e18fce17b58fbc644d7c9794de3b001dbf1d834
HareenUdayanath <hareenudayanath@gmail.com>
2016-04-12 02:29:24 +0530

    First

---
+++ DoctorSupportSystem/AddAppointmentsGUI/AddAppointment.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace DoctorSupportSystem.AddAppointmentsGUI
+{
+    partial class AddAppointment
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.calendar = new System.Windows.Forms.MonthCalendar();
+            this.selectedDate = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // calendar
+            // 
+            this.calendar.Location = new System.Drawing.Point(36, 137);
+            this.calendar.Name = "calendar";
+            this.calendar.TabIndex = 1;
+            this.calendar.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.calendar_DateChanged);
+            // 
+            // selectedDate
+            // 
+            this.selectedDate.AutoSize = true;
+            this.selectedDate.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.selectedDate.Location = new System.Drawing.Point(154, 71);
+            this.selectedDate.Name = "selectedDate";
+            this.selectedDate.Size = new System.Drawing.Size(44, 21);
+            this.selectedDate.TabIndex = 2;
+            this.selectedDate.Text = "Date";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Appointment Date:";
+            // 
+            // AddAppointment
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(452, 419);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.selectedDate);
+            this.Controls.Add(this.calendar);
+            this.Name = "AddAppointment";
+            this.Text = "AddAppointment";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MonthCalendar calendar;
+        private System.Windows.Forms.Label selectedDate;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ DoctorSupportSystem/AddAppointmentsGUI/AddAppointment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DoctorSupportSystem.AddAppointmentsGUI
+{
+    public partial class AddAppointment : Form
+    {
+        public AddAppointment()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void calendar_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            selectedDate.Text = calendar.SelectionStart.ToString().Split(' ')[0];
+        }
+    }
+}
