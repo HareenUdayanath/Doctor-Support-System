25c3a98335d3563730a6ca009ba50159e71c8179
HareenUdayanath <hareenudayanath@gmail.com>
2016-04-12 15:33:37 +0530

    After Add appointment

--- DoctorSupportSystem/Interfaces/AddAppointment.Designer.cs
+++ DoctorSupportSystem/Interfaces/AddAppointment.Designer.cs
@@ -31,11 +31,19 @@
             this.calendar = new System.Windows.Forms.MonthCalendar();
             this.selectedDate = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // calendar
             // 
-            this.calendar.Location = new System.Drawing.Point(36, 137);
+            this.calendar.Location = new System.Drawing.Point(9, 49);
             this.calendar.Name = "calendar";
             this.calendar.TabIndex = 1;
             this.calendar.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.calendar_DateChanged);
@@ -44,7 +52,7 @@
             // 
             this.selectedDate.AutoSize = true;
             this.selectedDate.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.selectedDate.Location = new System.Drawing.Point(154, 71);
+            this.selectedDate.Location = new System.Drawing.Point(107, 22);
             this.selectedDate.Name = "selectedDate";
             this.selectedDate.Size = new System.Drawing.Size(44, 21);
             this.selectedDate.TabIndex = 2;
@@ -53,22 +61,91 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(33, 76);
+            this.label1.Location = new System.Drawing.Point(6, 27);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(95, 13);
             this.label1.TabIndex = 3;
             this.label1.Text = "Appointment Date:";
             // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.calendar);
+            this.panel1.Controls.Add(this.selectedDate);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(15, 50);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(248, 220);
+            this.panel1.TabIndex = 4;
+            this.panel1.Tag = "H";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Choose the date";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 313);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Patient Name";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(113, 310);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(216, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(113, 355);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(216, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 358);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Patient NIC";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 416);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // AddAppointment
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(452, 419);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.selectedDate);
-            this.Controls.Add(this.calendar);
+            this.ClientSize = new System.Drawing.Size(351, 451);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
             this.Name = "AddAppointment";
-            this.Text = "AddAppointment";
+            this.Tag = "";
+            this.Text = "Add Appointment";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -78,5 +155,12 @@
         private System.Windows.Forms.MonthCalendar calendar;
         private System.Windows.Forms.Label selectedDate;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
     }
 }
