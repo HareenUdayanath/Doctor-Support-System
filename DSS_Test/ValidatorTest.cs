05ce65ab2e685b105197a032fe6529ca9a20ce50
HareenUdayanath <hareenudayanath@gmail.com>
2016-05-02 11:22:19 +0530

    After CS lecture

--- DSS_Test/ValidatorTest.cs
+++ DSS_Test/ValidatorTest.cs
@@ -5,12 +5,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DoctorSupportSystem.Help;
+using DoctorSupportSystem.Models;
 
 namespace DSS_Test
 {
+
+    /*
+    Unit tests for testing the Validator class in DoctorSupportSystem/Help. 
+        */
     [TestClass]
     public class ValidatorTest
     {
+
         [TestMethod]
         public void nicLessThan10()
         {
@@ -43,6 +49,94 @@ namespace DSS_Test
         {
             //Act
             bool result = Validator.nic("123456789A");
+            //Assert
+            Assert.AreEqual(false, result);
+        }
+
+        [TestMethod]
+        public void dateWith30DaysInFrb()
+        {
+            //Arrange
+            Date date = new Date(2000,02,30);
+            //Act
+            bool result = Validator.isValidDate(date);
+            //Assert
+            Assert.AreEqual(false, result);
+        }
+
+        [TestMethod]
+        public void dateWith31InApril()
+        {
+            //Arrange
+            Date date = new Date(2000, 04, 31);
+            //Act
+            bool result = Validator.isValidDate(date);
+            //Assert
+            Assert.AreEqual(false, result);
+        }
+
+        [TestMethod]
+        public void dateWith32InJanuary()
+        {
+            //Arrange
+            Date date = new Date(2000, 01, 32);
+            //Act
+            bool result = Validator.isValidDate(date);
+            //Assert
+            Assert.AreEqual(false, result);
+        }
+
+        [TestMethod]
+        public void dateWith29InFrbInLeepYear()
+        {
+            //Arrange
+            Date date = new Date(2000, 02, 29);
+            //Act
+            bool result = Validator.isValidDate(date);
+            //Assert
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod]
+        public void veryLateDate()
+        {
+            //Arrange
+            Date date = new Date(1000,02, 28);
+            //Act
+            bool result = Validator.isValidDate(date);
+            //Assert
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod]
+        public void veryLateDateOfBirth()
+        {
+            //Arrange
+            Date date = new Date(1000, 02, 28);
+            //Act
+            bool result = Validator.isValidDateOfBirth(date);
+            //Assert
+            Assert.AreEqual(false, result);
+        }
+
+        [TestMethod]
+        public void futureDate()
+        {
+            //Arrange
+            Date date = new Date(2020, 02, 28);
+            //Act
+            bool result = Validator.isValidDate(date);
+            //Assert
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod]
+        public void futureDateOfBirth()
+        {
+            //Arrange
+            Date date = new Date(2020, 02, 28);
+            //Act
+            bool result = Validator.isValidDateOfBirth(date);
             //Assert
             Assert.AreEqual(false, result);
         }
